82e6d34f4b8bd2c8a7c9a907f8ce2f60a496a5a0
Patrick Doyle <patrick.doyle@elastic.co>
2024-09-25 15:42:55 -0400

    Singular module name. Works.

--- distribution/tools/entitlement-agent/src/main/java/module-info.java
+++ distribution/tools/entitlement-agent/src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module org.elasticsearch.entitlements.agent {
+module org.elasticsearch.entitlement.agent {
     requires java.instrument;
     requires org.objectweb.asm;
     requires org.elasticsearch.entitlement.runtime;
