f2b907bf7bd8789185f0de84137a6fd2d74c12d3
Patrick Doyle <patrick.doyle@elastic.co>
2024-09-30 11:30:54 -0400

    Extract EntitlementChecks interface

--- libs/entitlement-runtime/src/main/java/org/elasticsearch/entitlement/runtime/api/EntitlementChecks.java
+++ libs/entitlement-runtime/src/main/java/org/elasticsearch/entitlement/runtime/api/EntitlementChecks.java
@@ -9,87 +9,15 @@
 
 package org.elasticsearch.entitlement.runtime.api;
 
-import java.util.Optional;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-
-import static java.util.Collections.newSetFromMap;
-import static org.elasticsearch.entitlement.runtime.api.FlagEntitlement.EXIT_JVM;
-import static org.elasticsearch.entitlement.runtime.internals.EntitlementInternals.isActive;
-
-public class EntitlementChecks {
-    boolean isAgentBooted = false;
-
-    private static final EntitlementChecks INSTANCE = new EntitlementChecks();
-
-    public static EntitlementChecks getInstance() {
-        return INSTANCE;
+public interface EntitlementChecks {
+    static EntitlementChecks getInstance() {
+        return EntitlementChecksImpl.INSTANCE;
     }
 
-    public void setAgentBooted() {
-        isAgentBooted = true;
-    }
+    void setAgentBooted();
+    boolean isAgentBooted();
 
-    public boolean isAgentBooted() {
-        return isAgentBooted;
-    }
+    void checkExitJvmEntitlement(Class<?> callerClass);
 
-    private final Set<Module> entitledToExit = newSetFromMap(new ConcurrentHashMap<>());
-
-    /**
-     * Causes entitlements to be checked. Before this is called, entitlements are not enforced,
-     * so there's no need for an application to set a lot of permissions which are required only during initialization.
-     */
-    public void activate() {
-        isActive = true;
-    }
-
-    public void revokeAll() {
-        entitledToExit.clear();
-    }
-
-    public boolean grant(Module module, Entitlement e) {
-        return switch (e) {
-            case FlagEntitlement __ -> entitledToExit.add(module);
-        };
-    }
-
-    private static Module requestingModule(Class<?> callerClass) {
-        Module callerModule = callerClass.getModule();
-        if (callerModule.getLayer() != ModuleLayer.boot()) {
-            // fast path
-            return callerModule;
-        }
-        int framesToSkip = 1  // getCallingClass (this method)
-            + 1  // the checkXxx method
-            + 1  // the runtime config method
-            + 1  // the instrumented method
-        ;
-        Optional<Module> module = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE)
-            .walk(
-                s -> s.skip(framesToSkip)
-                    .map(f -> f.getDeclaringClass().getModule())
-                    .filter(m -> m.getLayer() != ModuleLayer.boot())
-                    .findFirst()
-            );
-        return module.orElse(null);
-    }
-
-    private static boolean isTriviallyAllowed(Module requestingModule) {
-        return isActive == false || (requestingModule == null) || requestingModule == EntitlementChecks.class.getModule();
-    }
-
-    public void checkExitJvmEntitlement(Class<?> callerClass) {
-        // System.out.println("Checking for JVM Exit entitlement on " + callerClass.getSimpleName());
-        var requestingModule = requestingModule(callerClass);
-        if (isTriviallyAllowed(requestingModule)) {
-            // System.out.println(" - Trivially allowed");
-            return;
-        }
-        if (entitledToExit.contains(requestingModule)) {
-            // System.out.println(" - Granted");
-            return;
-        }
-        throw new NotEntitledException("Missing " + EXIT_JVM + " entitlement for " + requestingModule);
-    }
+    void activate();
 }
