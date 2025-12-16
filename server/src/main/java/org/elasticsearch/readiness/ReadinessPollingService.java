ae0c83e261ab161e3e0a8b5649db2565849f8519
Patrick Doyle <patrick.doyle@elastic.co>
2025-12-16 11:49:33 -0500

    Use multiple nodes in ReadinessPollingServiceTests

--- server/src/main/java/org/elasticsearch/readiness/ReadinessPollingService.java
+++ server/src/main/java/org/elasticsearch/readiness/ReadinessPollingService.java
@@ -148,6 +148,7 @@ class ReadinessPollingService {
             }
         };
 
+        logger.debug("initiating readiness poll");
         threadPool.generic().execute(attempt);
     }
 
--- server/src/test/java/org/elasticsearch/readiness/ReadinessPollingServiceTests.java
+++ server/src/test/java/org/elasticsearch/readiness/ReadinessPollingServiceTests.java
@@ -22,16 +22,26 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.EsExecutors;
 import org.elasticsearch.test.ClusterServiceUtils;
 import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.test.junit.annotations.TestLogging;
 import org.elasticsearch.test.transport.MockTransportService;
 import org.elasticsearch.threadpool.TestThreadPool;
 import org.elasticsearch.threadpool.ThreadPool;
+import org.elasticsearch.transport.TransportException;
+import org.elasticsearch.transport.TransportService;
 import org.junit.After;
 import org.junit.Before;
 
+import java.util.List;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.BooleanSupplier;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class ReadinessPollingServiceTests extends ESTestCase {
     /**
@@ -45,71 +55,128 @@ public class ReadinessPollingServiceTests extends ESTestCase {
      */
     private static final int QUICK_RETRY_MILLIS = 1;
 
+    /**
+     * This is for tests that <em>are</em> expecting a timeout to occur.
+     * Use a short value so we don't waste a lot of time.
+     */
+    private static final int QUICK_TIMEOUT_MILLIS = 10 * QUICK_RETRY_MILLIS;
+
     private ThreadPool threadPool;
-    private MockTransportService transport;
+    private MockTransportService sourceTransport;
+    private List<MockTransportService> targetTransports;
+    private List<DiscoveryNode> targetNodes;
     private ClusterService clusterService;
+    private ReadinessPollingService service;
 
     @Before
-    public void setup() {
+    public void setup() throws InterruptedException {
         threadPool = new TestThreadPool(getClass().getName());
-        transport = MockTransportService.createNewService(
+        sourceTransport = newMockTransportService();
+        targetTransports = IntStream.rangeClosed(1, 2).mapToObj(n -> newMockTransportService()).toList();
+        targetNodes = targetTransports.stream().map(TransportService::getLocalNode).toList();
+
+        // Connect all the target nodes to the source transport
+        for (MockTransportService target : targetTransports) {
+            CountDownLatch latch = new CountDownLatch(1);
+            sourceTransport.connectionManager().connectToNode(target.getLocalNode(), null, (x,y,listener)->{listener.onResponse(null);}, ActionListener.wrap(
+                connection -> latch.countDown(),
+                e -> fail("Unexpected exception connecting to target node: " + e)
+            ));
+            if (!latch.await(LONG_TIMEOUT_MILLIS, MILLISECONDS)) {
+                fail("Timed out waiting for target node to connect");
+            }
+        }
+
+        DiscoveryNodes.Builder nodesBuilder = DiscoveryNodes.builder()
+            .add(sourceTransport.getLocalNode());
+        targetNodes.forEach(nodesBuilder::add);
+        ClusterState clusterState = ClusterState.builder(new ClusterName("test-cluster")).nodes(nodesBuilder).build();
+        clusterService = ClusterServiceUtils.createClusterService(clusterState, threadPool);
+
+        // In general, the source node will always be ready
+        registerReadinessActionOnOneNode(sourceTransport, node -> true);
+    }
+
+    private MockTransportService newMockTransportService() {
+        MockTransportService newService = MockTransportService.createNewService(
             Settings.EMPTY,
             VersionInformation.CURRENT,
             TransportVersion.current(),
             threadPool
         );
-        transport.start();
-        transport.acceptIncomingRequests();
-        DiscoveryNode node = transport.getLocalNode();
-        DiscoveryNodes.Builder nodesBuilder = DiscoveryNodes.builder().add(node);
-        ClusterState clusterState = ClusterState.builder(new ClusterName("test-cluster")).nodes(nodesBuilder).build();
-        clusterService = ClusterServiceUtils.createClusterService(clusterState, threadPool);
+        newService.start();
+        newService.acceptIncomingRequests();
+        return newService;
     }
 
     @After
     public void teardown() {
         clusterService.close();
-        transport.close();
+        sourceTransport.close();
+        targetTransports.forEach(MockTransportService::close);
         terminate(threadPool);
     }
 
-    public void testReadinessSucceedsImmediately() throws Exception {
-        registerReadinessAction(() -> true);
+    public void testSuccessImmediately() throws Exception {
+        service = newReadinessPollingService(LONG_TIMEOUT_MILLIS);
+        registerReadinessActions(node -> true);
 
-        ReadinessPollingService poll = new ReadinessPollingService(
-            clusterService,
-            transport,
-            threadPool,
-            LONG_TIMEOUT_MILLIS,
-            QUICK_RETRY_MILLIS
-        );
-        AtomicBoolean readiness = new AtomicBoolean(false);
-
-        poll.execute(n -> true, ActionListener.wrap(readiness::set, e -> fail(e.toString())));
-
-        assertBusy(() -> assertTrue("Node was ready", readiness.get()));
+        assertReadiness(true);
     }
 
-    public void testReadinessRetriesThenSucceeds() throws Exception {
-        AtomicInteger remainingFailures = new AtomicInteger(2); // fails twice before success
-        registerReadinessAction(() -> remainingFailures.getAndDecrement() <= 0);
+    public void testSuccessAfterRetries() throws Exception {
+        service = newReadinessPollingService(LONG_TIMEOUT_MILLIS);
+        AtomicInteger remainingFailures = new AtomicInteger(2 * targetNodes.size()); // A couple of failures per node before success
+        registerReadinessActions(node -> remainingFailures.getAndDecrement() <= 0);
 
-        ReadinessPollingService poll = new ReadinessPollingService(
-            clusterService,
-            transport,
-            threadPool,
-            LONG_TIMEOUT_MILLIS,
-            QUICK_RETRY_MILLIS
-        );
-        AtomicBoolean readiness = new AtomicBoolean(false);
-
-        poll.execute(n -> true, ActionListener.wrap(readiness::set, e -> fail(e.toString())));
-
-        assertBusy(() -> assertTrue("Node was ready", readiness.get()));
+        assertReadiness(true);
     }
 
-    private void registerReadinessAction(BooleanSupplier readinessSupplier) {
-        TransportReadinessAction action = new TransportReadinessAction(new ActionFilters(Set.of()), null, Runnable::run, readinessSupplier);
+    public void testSuccessOnOneNode() throws Exception {
+        service = newReadinessPollingService(LONG_TIMEOUT_MILLIS);
+        var goodNode = randomFrom(targetNodes);
+        registerReadinessActions(goodNode::equals);
+
+        assertReadiness(true);
+    }
+
+    public void testTimeout() throws Exception {
+        service = newReadinessPollingService(QUICK_TIMEOUT_MILLIS);
+        registerReadinessActions(node -> false);
+
+        assertReadiness(false);
+    }
+
+    public void testTransportExceptionSameAsTimeout() throws Exception {
+        service = newReadinessPollingService(QUICK_TIMEOUT_MILLIS);
+        registerReadinessActions(node -> { throw new TransportException("test"); });
+
+        assertReadiness(false);
+    }
+
+    public void testIllegalStateExceptionSameAsTimeout() throws Exception {
+        service = newReadinessPollingService(QUICK_TIMEOUT_MILLIS);
+        registerReadinessActions(node -> { throw new IllegalStateException("test"); });
+
+        assertReadiness(false);
+    }
+
+    private ReadinessPollingService newReadinessPollingService(int quickTimeoutMillis) {
+        return new ReadinessPollingService(
+            clusterService,
+            sourceTransport,
+            threadPool,
+            quickTimeoutMillis,
+            QUICK_RETRY_MILLIS
+        );
+    }
+
+    private void registerReadinessActions(Predicate<DiscoveryNode> isReady) {
+        targetTransports.forEach(transport -> registerReadinessActionOnOneNode(transport, isReady));
+    }
+
+    private static void registerReadinessActionOnOneNode(MockTransportService transport, Predicate<DiscoveryNode> isReady) {
+        TransportReadinessAction action = new TransportReadinessAction(new ActionFilters(Set.of()), null, Runnable::run, ()->isReady.test(transport.getLocalNode()) );
         transport.registerRequestHandler(
             TransportReadinessAction.TYPE.name(),
             EsExecutors.DIRECT_EXECUTOR_SERVICE,
@@ -118,6 +185,12 @@ public class ReadinessPollingServiceTests extends ESTestCase {
             ReadinessRequest::new,
             (request, channel, task) -> action.execute(task, request, ActionListener.wrap(channel::sendResponse, channel::sendResponse))
         );
+    }
+
+    private void assertReadiness(Boolean expected) throws InterruptedException {
+        BlockingQueue<Boolean> readiness = new LinkedBlockingQueue<>();
+        service.execute(targetNodes::contains, ActionListener.wrap(readiness::add, e -> fail(e.toString())));
+        assertEquals(expected, readiness.poll(LONG_TIMEOUT_MILLIS, MILLISECONDS));
     }
 
 }
